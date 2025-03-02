3f35afb1d0b2879f556f985c389076f289056690
neurofumo <jasonramesdames@gmail.com>
2025-03-02 21:58:25 +0800

    v8.1 update

---
+++ scripts/changes.js
@@ -0,0 +1,132 @@
+export const VERSION_STRING = "Neuro Clicker v8.1"
+export const UPDATE_LOG =
+
+
+    '<div class="selectable">' +
+    '<div class="section">Info</div>' +
+    '<div class="subsection">' +
+    '<div class="title">About</div>' +
+    '<div class="listing">Cookie Clicker is a javascript game by <a href="https://orteil.dashnet.org" target="_blank">Orteil</a> and <a href=https:///dashnet.org" target="_blank">Opti</a>.</div>' +
+    '<div class="listing warning">Warning : clearing your browser cache or cookies will result in your save being wiped. Export your save and back it up first!</div>' +
+
+    '<div class="listing">Extra Credits (who worked on this mod):</div>' +
+    '<div class="listing">&bull; Superbox- Putting things together, technical management.</div>' +
+    '<div class="listing">&bull; Sandro- Some art (mainly borders and the big coin) and original idea.</div>' +
+    '<div class="listing">&bull; Velineste- Code.</div>' +
+    '<div class="listing">&bull; Xodabeef- Art, some code, release packaging, change logging (-> these things here).</div>' +
+    '<div class="listing">&bull; Zekava -Some additional art.</div>' +
+    '<div class="listing">&bull; Promote -Web version and domain hosting.</div>' +
+    '<div class="listing">&bull; Orteil is referenced enough here, so have Neuro instead: <a href="https://www.twitch.tv/vedal987" target="https://www.twitch.tv/vedal987"> https://www.twitch.tv/vedal987</a> .</div>' +
+
+    '<div class="listing">&bull; All dates in the new (modded) update log are to be in DD.MM.YYYY format.</div>' +
+
+    '</div><div class="subsection">' +
+    '<div class="title">Version history</div>' +
+
+    '</div><div class="subsection update">' +
+    '<div class="title">01.03.2025  - Neuro Clicker: v8.1</div>' +
+    '<div class="listing">&bull; Hotfixed the Hotfix (Mistakes in this section).</div>' +
+    '<div class="listing">&bull; Scheme unified to "Neuro Clicker v?.? [H for Hotfix]"</div>' +
+    '<div class="listing">&bull; Internal changes to make changelogging faster.</div>' +
+    '<div class="listing">&bull; Killed 3 Orteil changelogs that were still in here.</div>' +
+    '<div class="listing">&bull; Finally removed the Orteil Propaganda. Except for the credits, those are kinda needed so we\'re not completely illegal.</div>' +
+    '<div class="listing">&bull; Also forgot to ever mention Xoda being unbanned from NSH, months ago. (Yes, referring to myself in 3rd. As always.)</div>' +
+
+    '</div><div class="subsection update">' +
+    '<div class="title">01.03.2025  - Neuro Clicker: v8H</div>' +
+    '<div class="listing">&bull; Hotfixes for some bugs.</div>' +
+    '<div class="listing">&bull; Name unified to "Neuro Clicker".</div>' +
+
+    '</div><div class="subsection update">' +
+    '<div class="title">07.01.2025  - Nwero Clicker: v8</div>' +
+    '<div class="listing">&bull; Refactor all the code from main.js and index.html. (rare Superbox appearance here)</div>' +
+    '<div class="listing">&bull; Fixed a bug causing random garbage characters at the ends of upgrade quote strings in the process.</div>' +
+    '<div class="listing">&bull; Changed Temples to Harrisson temples.</div>' +
+    '<div class="listing">&bull; Made Mato happy.</div>' +
+    '<div class="listing">&bull; Changed more icons, as always.</div>' +
+    '<div class="listing">&bull; Added the (unimplemented) Kitchen Utensils\' Heavenly. What may those be?</div>' +
+    '<div class="listing">&bull; Local running is now done by a local server, due to refracture changes. This should affect only the small amount of people who run this locally.</div>' +
+
+    '</div><div class="subsection update">' +
+    '<div class="title">28.11.2024  - Nwero Clicker: v7_18_experimental</div>' +
+    '<div class="listing">&bull; Refactor all the code from main.js and index.html. (rare Superbox appearance here)</div>' +
+    '<div class="listing">&bull; Fixed a bug causing random garbage characters at the ends of upgrade quote strings in the process</div>' +
+
+    '</div><div class="subsection update">' +
+    '<div class="title">29.03.2024  - Nwero Clicker: v7_17</div>' +
+    '<div class="listing">&bull; Did a lot of code changes.</div>' +
+    '<div class="listing">&bull; NEURO CLICKER IS NOW WEB BASED AS WELL (thanks to Propmote) LET\'S FUCKING GOOO!!!</div>' +
+    '<div class="listing">&bull; Changed credits.</div>' +
+
+    '</div><div class="subsection update">' +
+    '<div class="title">29.03.2024  - Nwero Clicker: v7_16</div>' +
+    '<div class="listing">&bull; This update is a little smaller, redid name, descr. etc for banks, which are now gyms. The full redo is not finished yet, but most of the stuff you see when booting up the game is.</div>' +
+    '<div class="listing">&bull; Made a few more instances of cookie into neuro.</div>' +
+    '<div class="listing">&bull; Xoda also got herself banned from Neurocord AGAIN, this time perma, for posting a screenshot of :NeuroBwaa:. Make of that what you will.</div>' +
+
+    '</div><div class="subsection update">' +
+    '<div class="title">15.03.2024  - Nwero Clicker: v7_15</div>' +
+    '<div class="listing">&bull; Remade all easter egg sprites.</div>' +
+    '<div class="listing">&bull; Did all geometry triangles.</div>' +
+    '<div class="listing">&bull; Remade the wrath cookie.</div>' +
+    '<div class="listing">&bull; Remade the spell backgrounds.</div>' +
+    '<div class="listing">&bull; Changed some of the Mining Rig\'s building data.</div>' +
+    '<div class="listing">&bull; Xoda also got herself unbanned from Neurocord :NeuroHypers:</div>' +
+
+    '</div><div class="subsection update">' +
+    '<div class="title">06.03.2024  - Nwero Clicker: v7_14</div>' +
+    '<div class="listing">&bull; Added all missing milk.</div>' +
+    '<div class="listing">&bull; Fixed a lot of code issues left by Orteil.</div>' +
+    '<div class="listing">&bull; Changed model, building name and description for the Wheel Of Fortune (former Chancemaker).</div>' +
+    '<div class="listing">&bull; Made some more icons.</div>' +
+
+    '</div><div class="subsection update">' +
+    '<div class="title">26.02.2024  - Nwero Clicker: v7_13_Hotfix</div>' +
+    '<div class="listing">&bull; Fixed some accidentally made potentially offensive imagery.</div>' +
+
+    '</div><div class="subsection update">' +
+    '<div class="title">26.02.2024  - Nwero Clicker: v7_13</div>' +
+    '<div class="listing">&bull; The Bakery Is Now A Corpa!.</div>' +
+    '<div class="listing">&bull; Chnaged a bunch of icons, mainly Halloween related ones</div>' +
+    '<div class="listing">&bull; Fixed some more things.</div>' +
+    '<div class="listing">&bull; Changed some spell names.</div>' +
+    '<div class="listing">&bull; Got another team member (Zekava).</div>' +
+    '<div class="listing">&bull; Xoda (me) got herself banned from the Discord.</div>' +
+
+    '</div><div class="subsection">' +
+    '</div><div class="subsection update">' +
+    '<div class="title">24.02.2024  - Nwero Clicker: v7_12</div>' +
+    '<div class="listing">&bull; Made Valentines Milk be selected automatically on Valentines Day (or if you activate the respective season switch)</div>' +
+    '<div class="listing">&bull; Fixed some things.</div>' +
+    '<div class="listing">&bull; Edited an achievement.</div>' +
+
+    '</div><div class="subsection update">' +
+    '<div class="title">13.02.2024  - Nwero Clicker: v7_11 - The Valentines Update</div>' +
+    '<div class="listing">&bull; Changed Season Timing again.</div>' +
+    '<div class="listing">&bull; Added Valentines Milk, because i can.</div>' +
+    '<div class="listing">&bull; Finished changing all things related to Valentines, at least as far as i can see.</div>' +
+    '<div class="listing">&bull; Happy Valentines!</div>' +
+
+    '</div><div class="subsection update">' +
+    '<div class="title">12.02.2024  - Nwero Clicker: v7_10</div>' +
+    '<div class="listing">&bull; Fixed Neuro Storms.</div>' +
+    '<div class="listing">&bull; Changed the Version Display.</div>' +
+    '<div class="listing">&bull; Fucked up Save Files with that lmao.</div>' +
+    '<div class="listing">&bull; Changed Season timing.</div>' +
+    '<div class="listing">&bull; Remade the Ascention Number Panel graphic so it looks good.</div>' +
+    '<div class="listing">&bull; Uhh some other stuff i forgor :skull: .</div>' +
+
+    '</div><div class="subsection update">' +
+    '<div class="title">12.02.2024  - Nwero Clicker: v7_9</div>' +
+    '<div class="listing">&bull; I have now decided to make my own Update Log Entries with every Version i make. There is no real schedule or anything, i just zip up new versions whenever someone asks for a download after i change something.</div>' +
+    '<div class="listing">&bull; By now I have changed a few graphics, done what is written in the older Update Log, and added this. I have also edited the previous entry, putting the credits of who helped with this mod above the Update Log and into the About Header.</div>' +
+    '<div class="listing">&bull; I will not list what i have done up until now in detail as it is just unnescessary. I spelt that wrong.</div>' +
+    '<div class="listing">&bull; Also, thanks to SuperBox, the Twitch Link is now an actual link. Thank you for reading!</div>' +
+
+    '</div><div class="subsection update">' +
+    '<div class="title">08.02.2024  - Nwero Clicker: An Update Log Entry</div>' +
+    '<div class="listing">&bull; Hello, I am Xodabeef; a part of the Team behind this Cookie Clicker Mod.</div>' +
+    '<div class="listing">&bull; I am the main Artist, but i am here in the code now too. The reason is simple: we wanted to cut back on unnecessary files (Orteil left a LOT of shit in here). That made me look for references to the Dungeon Minigame in the code. It is not referenced anywhere, meaning i can (and likely have when you read this) delete all files related to the Dungeon Minigame, to both make this smaller and have a more clear estimate on how much is still to be done.</div>' +
+    '<div class="listing">&bull; While looking for these references, i found out about the way these comments work. And because of how easy it is (and out of boredom), i decided to put our names in here.</div>' +
+    '<div class="listing">&bull; I have also removed some of the older changelogs, since they are not necesary.</div>' +
+;
