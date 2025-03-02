d48826ce0a60c5d44aaa7c526ca2069eb05987de
neurofumo <jasonramesdames@gmail.com>
2025-03-02 21:50:50 +0800

    v8.1 update

--- scripts/changes.js
+++ scripts/changes.js
@@ -1,4 +1,4 @@
-export const VERSION_STRING = "Modified (Neuro Clicker v8H)"
+export const VERSION_STRING = "Neuro Clicker v8.1"
 export const UPDATE_LOG =
 
 
@@ -7,17 +7,7 @@ export const UPDATE_LOG =
     '<div class="subsection">' +
     '<div class="title">About</div>' +
     '<div class="listing">Cookie Clicker is a javascript game by <a href="https://orteil.dashnet.org" target="_blank">Orteil</a> and <a href=https:///dashnet.org" target="_blank">Opti</a>.</div>' +
-    //'<div class="listing">We have an <a href="https://discordapp.com/invite/cookie" target="_blank">official Discord</a>, as well as a <a href="http://forum.dashnet.org" target="_blank">forum</a>; '+
-    '<div class="listing">We have an <a href="https://discordapp.com/invite/cookie" target="_blank">official Discord</a>; ' +
-    'if you\'re looking for help, you may also want to visit the <a href="https://www.reddit.com/r/CookieClicker" target="_blank">subreddit</a> ' +
-    'or the <a href="https://cookieclicker.wikia.com/wiki/Cookie_Clicker_Wiki" target="_blank">wiki</a>.</div>' +
-    '<div class="listing">News and teasers are usually posted on my <a href="https://orteil42.tumblr.com/" target="_blank">tumblr</a> and <a href=https:///twitter.com/orteil42" target="_blank">twitter</a>.</div>' +
-    '<div class="listing" id="supportSection"><b style="color:#fff;opacity:1;">Cookie Clicker is 100% free, forever.</b> Want to support us so we can keep developing games? Here\'s some ways you can help :<div style="margin:4px 12px;line-height:150%;">' +
-    '<br>&bull; support us on <a href="https://www.patreon.com/dashnet" target="_blank" class="highlightHover" style="background:#f86754;box-shadow:0 0 0 1px #c52921 inset,0 2px 0 #ff966d inset;text-shadow:0 -1px 0 #ff966d,0 1px 0 #c52921;text-decoration:none;color:#fff;font-weight:bold;padding:1px 4px;">Patreon</a> <span style="opacity:0.5;">(there\'s perks!)</span>' +
-    '<br>&bull; <form target="_blank" action="https://www.paypal.com/cgi-bin/webscr" method="post" id="donate"><input type="hidden" name="cmd" value="_s-xclick"><input type="hidden" name="hosted_button_id" value="BBN2WL3TC6QH4"><input type="submit" id="donateButton" value="donate" name="submit" alt="PayPal — The safer, easier way to pay online."><img alt="" src="https://www.paypalobjects.com/nl_NL/i/scr/pixel.gif" width="1" height="1"></form> to our PayPal' +
-    '<br>&bull; disable your adblocker<br>&bull; check out our <a href="https://www.redbubble.com/people/dashnet" target="_blank">rad cookie shirts, hoodies and stickers</a>!<br>&bull; (if you want!)</div></div>' +
-    '<div class="listing warning">Note : if you find a new bug after an update and you\'re using a 3rd-party add-on, make sure it\'s not just your add-on causing it!</div>' +
-    '<div class="listing warning">Warning : clearing your browser cache or cookies <small>(what else?)</small> will result in your save being wiped. Export your save and back it up first!</div>' +
+    '<div class="listing warning">Warning : clearing your browser cache or cookies will result in your save being wiped. Export your save and back it up first!</div>' +
 
     '<div class="listing">Extra Credits (who worked on this mod):</div>' +
     '<div class="listing">&bull; Superbox- Putting things together, technical management.</div>' +
@@ -34,9 +24,18 @@ export const UPDATE_LOG =
     '<div class="title">Version history</div>' +
 
     '</div><div class="subsection update">' +
-    '<div class="title">07.01.2025  - Neuro Clicker: v8H</div>' +
-    '<div class="listing">&bull; Hotfixes for some bugs</div>' +
-    '<div class="listing">&bull; Name unified to "Neuro Clicker"</div>' +
+    '<div class="title">01.03.2025  - Neuro Clicker: v8.1</div>' +
+    '<div class="listing">&bull; Hotfixed the Hotfix (Mistakes in this section).</div>' +
+    '<div class="listing">&bull; Scheme unified to "Neuro Clicker v?.? [H for Hotfix]"</div>' +
+    '<div class="listing">&bull; Internal changes to make changelogging faster.</div>' +
+    '<div class="listing">&bull; Killed 3 Orteil changelogs that were still in here.</div>' +
+    '<div class="listing">&bull; Finally removed the Orteil Propaganda. Except for the credits, those are kinda needed so we\'re not completely illegal.</div>' +
+    '<div class="listing">&bull; Also forgot to ever mention Xoda being unbanned from NSH, months ago. (Yes, referring to myself in 3rd. As always.)</div>' +
+
+    '</div><div class="subsection update">' +
+    '<div class="title">01.03.2025  - Neuro Clicker: v8H</div>' +
+    '<div class="listing">&bull; Hotfixes for some bugs.</div>' +
+    '<div class="listing">&bull; Name unified to "Neuro Clicker".</div>' +
 
     '</div><div class="subsection update">' +
     '<div class="title">07.01.2025  - Nwero Clicker: v8</div>' +
@@ -130,70 +129,4 @@ export const UPDATE_LOG =
     '<div class="listing">&bull; I am the main Artist, but i am here in the code now too. The reason is simple: we wanted to cut back on unnecessary files (Orteil left a LOT of shit in here). That made me look for references to the Dungeon Minigame in the code. It is not referenced anywhere, meaning i can (and likely have when you read this) delete all files related to the Dungeon Minigame, to both make this smaller and have a more clear estimate on how much is still to be done.</div>' +
     '<div class="listing">&bull; While looking for these references, i found out about the way these comments work. And because of how easy it is (and out of boredom), i decided to put our names in here.</div>' +
     '<div class="listing">&bull; I have also removed some of the older changelogs, since they are not necesary.</div>' +
-
-    '</div><div class="subsection update small">' +
-    '<div class="title">01/04/2019 - 2.019 (the "this year" update)</div>' +
-    '<div class="listing">&bull; game has been renamed to "Cookie Clicker" to avoid confusion</div>' +
-    '<div class="listing">&bull; can now click the big cookie to generate cookies for free</div>' +
-    '<div class="listing">&bull; removed fall damage</div>' +
-    '<div class="listing">&bull; fixed various typos : player\'s name is now correctly spelled as "[bakeryName]"</div>' +
-    '<div class="listing">&bull; removed all references to computer-animated movie <i style="font-style:italic;">Hoodwinked!</i> (2005)</div>' +
-    '<div class="listing">&bull; went back in time and invented cookies and computer mice, ensuring Cookie Clicker would one day come to exist</div>' +
-    '<div class="listing">&bull; game now fully compliant with Geneva Conventions</div>' +
-    '<div class="listing">&bull; dropped support for TI-84 version</div>' +
-    '<div class="listing">&bull; released a low-res retro version of the game, playable here : <a href="https://orteil.dashnet.org/experiments/cookie/" target="_blank">orteil.dashnet.org/experiments/cookie</a></div>' +
-    '<div class="listing">&bull; updated version number</div>' +
-
-    '</div><div class="subsection update small">' +
-    '<div class="title">05/03/2019 - cookies for days</div>' +
-    '<div class="listing">&bull; added over 20 new cookies, all previously suggested by our supporters on <a href="https://www.patreon.com/dashnet" target="_blank">Patreon</a></div>' +
-    '<div class="listing">&bull; added 2 heavenly upgrades</div>' +
-    '<div class="listing">&bull; the Golden goose egg now counts as a golden cookie upgrade for Residual luck purposes</div>' +
-    '<div class="listing">&bull; golden sugar lumps now either double your cookies, or give you 24 hours of your CpS, whichever is lowest (previously was doubling cookies with no cap)</div>' +
-    '<div class="listing">&bull; the amount of heralds is now saved with your game, and is used to compute offline CpS the next time the game is loaded; previously, on page load, the offline calculation assumed heralds to be 0</div>' +
-    '<div class="listing">&bull; added a system to counteract the game freezing up (and not baking cookies) after being inactive for a long while on slower computers; instead, this will now trigger sleep mode, during which you still produce cookies as if the game was closed; to enable this feature, use the "Sleep mode timeout" option in the settings</div>' +
-    '<div class="listing">&bull; vaulting upgrades is now done with shift-click, as ctrl-click was posing issues for Mac browsers</div>' +
-    '<div class="listing">&bull; made tooltips for building CpS boosts from synergies hopefully clearer</div>' +
-    '<div class="listing">&bull; fixed an exploit with gambler\'s fever dream working across exports and ascensions</div>' +
-    '<div class="listing">&bull; can now hide tooltips in the garden by keeping the shift key pressed to make it easier to see where you\'re planting</div>' +
-    '<div class="listing">&bull; fixed a bug with golden cookies/reindeer not disappearing properly in some circumstances</div>' +
-    '<div class="listing">&bull; the Dragon\'s Curve aura should now properly make sugar lumps twice as weird</div>' +
-    '<div class="listing">&bull; the ctrl key should less often register incorrectly as pressed</div>' +
-    '<div class="listing">&bull; added a new ad slot in the top-right, as while our playerbase is strong and supportive as ever, our ad revenue sometimes fluctuates badly; we may remove the ad again should our income stabilize</div>' +
-    '<div class="listing">&bull; made a few adjustments to make the game somewhat playable in mobile browsers; it\'s not perfect and can get buggy, but it\'s functional! (you may need to zoom out or scroll around to view the game properly)</div>' +
-    '<div class="listing">&bull; speaking of which, we also got some good progress on the mobile app version (built from scratch for mobile), so stay tuned!</div>' +
-
-    '</div><div class="subsection update">' +
-    '<div class="title">25/10/2018 - feedback loop</div>' +
-    '<div class="listing">&bull; added a new building</div>' +
-    '<div class="listing">&bull; launched our <a href="https://www.patreon.com/dashnet" class="orangeLink" target="_blank">Patreon</a> <span style="font-size:80%;">(the link is orange so you\'ll notice it!)</span></div>' +
-    '<div class="listing">&bull; added a bunch of new heavenly upgrades, one of which ties into our Patreon but benefits everyone (this is still experimental!)</div>' +
-    '<div class="listing">&bull; when hovering over grandmas, you can now see their names and ages</div>' +
-    '<div class="listing">&bull; "make X cookies just from Y" requirements are now higher</div>' +
-    '<div class="listing">&bull; tweaked the prices of some heavenly upgrades to better fit the current cookie economy (it turns out billions of heavenly chips is now very achievable)</div>' +
-    '<div class="listing">&bull; building tooltips now display what % of CpS they contribute through synergy upgrades</div>' +
-    '<div class="listing">&bull; queenbeets now give up to 4% of gym, down from 6%</div>' +
-    '<div class="listing">&bull; among other things, season switches now display how many seasonal upgrades you\'re missing, and permanent upgrade slots now display the name of the slotted upgrade</div>' +
-    '<div class="listing">&bull; season switches have reworked prices</div>' +
-    '<div class="listing">&bull; season switches can now be cancelled by clicking them again</div>' +
-    '<div class="listing">&bull; can no longer accidentally click wrinklers through other elements</div>' +
-    '<div class="listing">&bull; sugar frenzy now triples your CpS for an hour instead of doubling it</div>' +
-    '<div class="listing">&bull; this text is now selectable</div>' +
-    '<div class="listing">&bull; progress on dungeons minigame is still very much ongoing</div>' +
-
-    '</div><div class="subsection update small">' +
-    '<div class="title">08/08/2018 - hey now</div>' +
-    '<div class="listing">&bull; Cookie Clicker somehow turns 5, going against doctors\' most optimistic estimates</div>' +
-    '<div class="listing">&bull; added a new tier of building achievements, all named after Smash Mouth\'s classic 1999 hit "All Star"</div>' +
-    '<div class="listing">&bull; added a new tier of building upgrades, all named after nothing in particular</div>' +
-    '<div class="listing">&bull; <b>to our players :</b> thank you so much for sticking with us all those years and allowing us to keep making the dumbest game known to mankind</div>' +
-    '<div class="listing">&bull; resumed work on the dungeons minigame</div>' +
-
-    '</div><div class="subsection update">' +
-    '<div class="title">08/08/2013 - game launch</div>' +
-    '<div class="listing">&bull; made the game in a couple hours, for laughs</div>' +
-    '<div class="listing">&bull; kinda starting to regret it</div>' +
-    '<div class="listing">&bull; ah well</div>' +
-    '</div>' +
-    '</div>'
 ;
